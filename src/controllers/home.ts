eea7bbc663d1718b459a8d8be01b0a8e5771d142
Napoleon <sherlockdev0105@gmail.com>
2021-01-09 00:25:51 +0900

    update user model and signup function

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,5 +1,5 @@
-import { Request, Response } from "express";
-
+import { NextFunction, Request, Response } from "express";
+import { User, UserDocument, AuthToken } from "../models/User";
 /**
  * GET /
  * Home page.
@@ -9,3 +9,22 @@ export let index = (req: Request, res: Response) => {
     title: "Home"
   });
 };
+
+
+export let getUserList = (req: Request, res: Response, next: NextFunction) => {
+  let scopeCategory = req.user.scope_category;
+  let scopeType = req.user.scope_type;
+  User.find({ $and: [{ scope_category: { $in: scopeCategory }, scope_type: { $in: scopeType } }] }, function (err, user) {
+    if(err){
+      return next(err);
+    }
+    res.send(user);
+  })
+}
+
+export let getPostListByUser = (req: Request, res: Response) => {
+  
+}
+export let postOneData = (req: Request, res: Response) => {
+
+}
