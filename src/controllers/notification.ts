58e80beadd82669f5520f5e54f10582ab8fcdf4d
Napoleon <sherlockdev0105@gmail.com>
2021-01-14 22:54:40 +0900

    update getnotification apis

--- src/controllers/notification.ts
+++ src/controllers/notification.ts
@@ -8,81 +8,112 @@ export let sendLikeNotification = (req: Request, res: Response, next: NextFuncti
     let user_id = req.body.user_id;
     let post_id = req.body.post_id;
     let receiver_id;
-    
+
     Post.findById(post_id, (err, post) => {
         if (err) { return next(err); }
-        receiver_id =  post.user_id;
+        receiver_id = post.user_id;
         post.total_likes += 1;
         post.save();
-    })
-
-    const like = new PostLike({
-        sender_id: user_id,
-        receiver_id: receiver_id,
-        post_id: post_id
-    });
-    like.save((err) => {
-        if (err) { return next(err); }
-        res.status(200);
-        res.json({
-            result: "Sent like notification",
-            data: like
+        const like = new PostLike({
+            sender_id: user_id,
+            receiver_id: receiver_id,
+            post_id: post_id
         });
-    });
+        like.save((err) => {
+            if (err) { return next(err); }
+            res.status(200);
+            res.json({
+                result: "Sent like notification",
+                data: like
+            });
+        });
+    })
 }
 
 export let sendCommentNotification = (req: Request, res: Response, next: NextFunction) => {
     let user_id = req.body.user_id;
     let post_id = req.body.post_id;
     let content = req.body.content;
-    let receiver_id;
+    let receiver_id = '';
     Post.findById(post_id, (err, post) => {
         if (err) { return next(err); }
         receiver_id = post.user_id;
         post.total_comments += 1;
         post.save();
-    })
-
-    const comment = new PostComment({
-        sender_id: user_id,
-        post_id: post_id,
-        content: content
-    });
-
-    comment.save((err) => {
-        if (err) { return next(err); }
-        res.status(200);
-        res.json({
-            result: "Sent comment notification",
-            data: comment
+        const comment = new PostComment({
+            sender_id: user_id,
+            receiver_id: receiver_id,
+            post_id: post_id,
+            content: content
         });
-    });
+
+        comment.save((err) => {
+            if (err) { return next(err); }
+            res.status(200);
+            res.json({
+                result: "Sent comment notification",
+                data: comment
+            });
+        });
+    })
 }
 
 export let getCommentNotifications = (req: Request, res: Response, next: NextFunction) => {
-    // let user_id = req.body.user_id;
-    // let notifications = [];
-    // PostComment.find({receiver_id: user_id}, (err, notifications) => {
-    //     if (err) { return next(err); }
-    //     notifications.forEach((notification) =>{
-    //         Post.findById(notification.post_id, (err, post) =>{
-    //             if (err) { return next(err); }
-    //             notification.image = post.image;
-
-    //         })
-    //     })
-        
-    // })
-    // res.send(notifications);
+    let user_id = req.body.user_id;
+    PostComment.find({ receiver_id: user_id }, (err, results) => {
+        if (err) { return next(err); }
+        let notifications = Array();
+        results.forEach((item) => {
+            Post.findById(item.post_id, (err, post) => {
+                if (err) { return next(err); }
+                let sender_img;
+                User.findById(item.sender_id, (err, user) => {
+                    if (err) { return next(err); }
+                    sender_img = user.profile.picture;
+                    let jsonSTR = JSON.stringify(item);
+                    item = JSON.parse(jsonSTR);
+                    notifications.push({
+                        ...item,
+                        user_image: sender_img,
+                        post_image: post.image
+                    });
+                    if (notifications.length === results.length) {
+                        res.json({
+                            data: notifications
+                        });
+                    }
+                })
+            })
+        })
+    })
 }
 
 export let getLikeNotifications = (req: Request, res: Response, next: NextFunction) => {
     let user_id = req.body.user_id;
-
-    PostLike.find({receiver_id: user_id}, (err, notifications) => {
+    PostLike.find({ receiver_id: user_id }, (err, results) => {
         if (err) { return next(err); }
-        res.json({
-            data: notifications
-        });
+        let notifications = Array();
+        results.forEach((item) => {
+            Post.findById(item.post_id, (err, post) => {
+                if (err) { return next(err); }
+                let sender_img;
+                User.findById(item.sender_id, (err, user) => {
+                    if (err) { return next(err); }
+                    sender_img = user.profile.picture;
+                    let jsonSTR = JSON.stringify(item);
+                    item = JSON.parse(jsonSTR);
+                    notifications.push({
+                        ...item,
+                        user_image: sender_img,
+                        post_image: post.image
+                    });
+                    if (notifications.length === results.length) {
+                        res.json({
+                            data: notifications
+                        });
+                    }
+                })
+            })
+        })
     })
 }
